60c6a6744bd28e64f848f42db099e38130aec8b9
MZkhan <mdzamakhan18@gmail.com>
2021-05-23 07:15:47 -0700

    added first file

---
+++ Java Programming/src/Programms/basics_a.java
@@ -0,0 +1,15 @@
+package Programms;
+
+import java.util.Scanner;
+
+public class basics_a {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter basic salary of Ramesh:");
+		double bsal = sc.nextDouble();
+		double da = bsal * 0.4D;
+		double hr = bsal * 0.2D;
+		double gsal = da + hr + bsal;
+		System.out.println("Gross Salary of Ramesh is : " + gsal);
+	}
+}
